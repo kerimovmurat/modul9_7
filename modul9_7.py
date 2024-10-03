284fd01bb5c3dd0fbbaf08f44aed343d9071ee36
kerimovmurat <kerimovmurat@yandex.ru>
2024-10-03 15:22:25 +0300

    Update modul9_7.py

--- modul9_7.py
+++ modul9_7.py
@@ -4,6 +4,15 @@ def is_prime(func):
         if result > 1:
             for i in range(2, int(result ** 0.5) + 1):
                 if result % i == 0:
+                    print('Составное')
+                    return result
+                break
+            print('Простое')
+        else:
+            print('Составное')
+        return result
+    return wrapper
+
 
 
 @is_prime
@@ -12,5 +21,5 @@ def sum_three(a, b, c):
 
 
 # Пример использования:
-result = sum_three (2, 3, 6)
-print (result)
+result = sum_three(2, 3, 6)
+print(result)
